8f5a99feb71847dcabe5275205359361af687bc3
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-28 14:22:33 -0600

    made past cities buttons and added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var submitBtn = document.querySelector('.btn');
+var submitBtn = document.querySelector('.btn-submit');
 var cityInput = document.querySelector('input');
 var cityName = cityInput.value;
 var pastCityNames= [];
@@ -6,10 +6,30 @@ var pastCityColumnEl = document.querySelector(".past-city")
 
 function addPastCity(){
     for(var i = 0; i < pastCityNames.length; i++){
-        var pastCityEl = document.createElement("h4");
+        var pastCityEl = document.createElement("button");
+        var lineBreak = document.createElement("br");
+        pastCityEl.setAttribute("class", "btn btn-outline-primary");
         pastCityEl.textContent = pastCityNames[i];
+        pastCityColumnEl.append(lineBreak);
         pastCityColumnEl.append(pastCityEl);
     }
+
+    storeCities();
+}
+
+function storeCities(){
+    localStorage.setItem("storedCityNames", JSON.stringify(pastCityNames));
+}
+
+function init(){
+    var storedCities = JSON.parse(localStorage.getItem("storedCityNames"));
+    if (storedCities != null){
+        pastCityNames = storedCities;
+        pastCityColumnEl.innerHTML= "";
+        addPastCity();
+    }
+
+
 }
 
 submitBtn.addEventListener("click", function(){
@@ -21,3 +41,4 @@ submitBtn.addEventListener("click", function(){
     addPastCity();
 });
 
+init();
